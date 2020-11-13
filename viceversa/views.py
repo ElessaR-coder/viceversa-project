50e15aab5e492b66a74c83a9ee17ccb1fc8507e9
elessar <soul.alex.777@gmail.com>
2020-11-13 21:04:36 +0700

    Revert "Add words count feature"
    This reverts commit c6d293e50d37d5e7957b4440bf62c1697854d5c4.

--- viceversa/views.py
+++ viceversa/views.py
@@ -6,5 +6,4 @@ def home(request):
 def reverse(request):
 	user_text = request.GET["usertext"]
 	reversed_text = user_text[::-1]
-	n = len(user_text.split())
-	return render(request, "reverse.html", {"usertext" : user_text, "reversedtext" : reversed_text, "n" : n})
+	return render(request, "reverse.html", {"usertext" : user_text, "reversedtext" : reversed_text})
